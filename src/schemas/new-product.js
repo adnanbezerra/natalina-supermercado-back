4ab7af012ad43ff386b68dfaed9a5354f64a13ab
adnan bezerra <adnanbezerra@proton.me>
2024-11-26 11:17:16 -0300

    refactor/remove description from product creation

--- src/schemas/new-product.js
+++ src/schemas/new-product.js
@@ -3,6 +3,5 @@ import joi from "joi";
 export const NewProductSchema = joi.object({
     name: joi.string().required(),
     price: joi.number().required(),
-    description: joi.string().required(),
     image: joi.string().required(),
 });
