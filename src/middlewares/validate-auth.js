f132c282cd1b9e0321c1a6c3b285a6519a987421
Layla <laylaramosapolinario@gmail.com>
2024-12-06 23:13:08 -0300

    Adicionando rota get para usuários

--- src/middlewares/validate-auth.js
+++ src/middlewares/validate-auth.js
@@ -1,30 +1,39 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import db from "../database/mongodb.js";
+
 dotenv.config();
 
 export async function validatingToken(req, res, next) {
     try {
         const { authorization } = req.headers;
-        const token = authorization?.replace("Bearer ", "");
+        if (!authorization) {
+            return res.status(401).send("Token não fornecido.");
+        }
+
+        const token = authorization.replace("Bearer ", "");
         if (!token) {
-            return res.sendStatus(401);
+            return res.status(401).send("Token inválido ou ausente.");
         }
-        const data = jwt.verify(token, process.env.JWT_SECRET);
-        if (data) {
-            const userId = await db.collection("users").findOne({ userId });
 
-            if (!userId) {
-                return res.sendStatus(404);
-            }
-
-            res.locals.userId = userId;
-
-            next();
-        } else {
-            return res.status(401).send("Erro ao validar o usuário");
+        // Verifica o token JWT
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decoded || !decoded.id) {
+            return res.status(401).send("Token inválido.");
         }
+
+        // Busca o usuário no banco de dados
+        const user = await db.collection("users").findOne({ _id: decoded.id });
+        if (!user) {
+            return res.status(404).send("Usuário não encontrado.");
+        }
+
+        // Armazena o usuário na requisição para os próximos middlewares
+        res.locals.user = user;
+
+        next(); // Continua o fluxo
     } catch (error) {
-        return res.sendStatus(500);
+        console.error("Erro na validação do token:", error);
+        return res.status(500).send("Erro interno no servidor.");
     }
 }
--- src/routers/users-routers.js
+++ src/routers/users-routers.js
@@ -3,6 +3,7 @@ import { cadastroUser } from "../controllers/cadastro-user-controller.js";
 import { deleteUserController } from "../controllers/delete-user-controller.js";
 import { editUserController } from "../controllers/edit-user-controller.js";
 import { loginUserController } from "../controllers/login-user-controller.js";
+import { getUserController } from "../controllers/user-controller.js";
 
 export const UsersRouter = Router();
 
@@ -10,3 +11,4 @@ UsersRouter.post("/user/register", cadastroUser);
 UsersRouter.delete("/user/:id", deleteUserController);
 UsersRouter.put("/user/:id", editUserController);
 UsersRouter.post("/user/login", loginUserController);
+UsersRouter.get("/user/:id", getUserController);
